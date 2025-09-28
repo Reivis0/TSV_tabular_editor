5d161cdc0e24096a38225c819b109618e7ae6872
Revis0 <anna.sevvost@gmail.com>
2025-09-29 00:29:28 +0300

    The main nteface was added

---
+++ src/include/mainwindow.h
@@ -0,0 +1,39 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QPushButton>
+#include <QMenu>
+#include <QToolButton>
+#include <QToolBar>
+#include <QTableView>
+#include <QStandardItemModel>
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+private:
+  void initializeMenu();
+  void initializeButtons();
+  void initializeTable();
+
+  QPushButton * m_btn1;
+  QPushButton * m_btn2;
+  QPushButton * m_btn3;
+  QPushButton * m_btn4;
+
+  QMenu * m_menu1;
+  QMenu * m_menu2;
+  QToolButton *m_dropDownBtn1;
+  QToolButton *m_dropDownBtn2;
+
+  QToolBar * m_toolbar;
+  QStandardItemModel * m_tableModel;
+  QTableView * m_table;
+
+};
+#endif // MAINWINDOW_H
